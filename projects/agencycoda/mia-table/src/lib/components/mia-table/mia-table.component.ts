b42b5e11817bdae1cea62387357d4484ab1d3341
Matias Camiletti <matias.camiletti@gmail.com>
2021-03-20 13:47:02 -0300

    Create first structure for the first columns. Install angular material.

--- projects/agencycoda/mia-table/src/lib/components/mia-table/mia-table.component.ts
+++ projects/agencycoda/mia-table/src/lib/components/mia-table/mia-table.component.ts
@@ -1,11 +1,13 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, Input, OnInit } from '@angular/core';
 import { TableAnimation } from '../../animations/table-animation';
 import { MiaTableConfig } from '../../entities/mia-table-config';
+import { MiaPagination } from '@agencycoda/mia-core';
 
 @Component({
   selector: 'mia-table',
   templateUrl: './mia-table.component.html',
-  styleUrls: ['./mia-table.component.css'],
+  styleUrls: ['./mia-table.component.scss'],
   animations: [
     TableAnimation.componentAnimation
   ]
@@ -14,9 +16,42 @@ export class MiaTableComponent implements OnInit {
 
   @Input() config = new MiaTableConfig();
 
+  selection = new SelectionModel<any>(true, [], true);
+  dataItems?: MiaPagination<any>;
+  displayColumns: Array<String> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.processDisplayColumns();
+    this.loadMocks();
   }
 
+  processDisplayColumns() {
+    this.displayColumns = new Array<String>();
+    for (const column of this.config.columns) {
+      this.displayColumns.push(column.key);
+    }
+  }
+
+  loadMocks() {
+    this.dataItems = {
+      current_page: 1,
+      first_page_url: '',
+      from: '',
+      last_page: 1,
+      last_page_url: '',
+      next_page_url: '',
+      path: '',
+      per_page: 50,
+      prev_page_url: '',
+      to: '',
+      total: 10,
+      data: [
+        {
+          
+        }
+      ]
+    };
+  }
 }
--- projects/agencycoda/mia-table/src/lib/mia-table.module.ts
+++ projects/agencycoda/mia-table/src/lib/mia-table.module.ts
@@ -1,12 +1,23 @@
 import { MiaCoreModule } from '@agencycoda/mia-core';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MiaTableComponent } from './components/mia-table/mia-table.component';
 import { StringColumnComponent } from './columns/string-column/string-column.component';
 
+/** Angular Material */
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [MiaTableComponent, StringColumnComponent],
   imports: [
-    MiaCoreModule
+    BrowserModule,
+    MiaCoreModule,
+
+    // Angular Material
+    MatIconModule,
+    MatTableModule
   ],
   exports: [
     MiaTableComponent
