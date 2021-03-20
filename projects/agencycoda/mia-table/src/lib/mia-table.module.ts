e8574f4572281c68f1ad90329811dfaef4d69b92
Matias Camiletti <matias.camiletti@gmail.com>
2021-03-20 12:58:07 -0300

    Create structure initial.

---
+++ projects/agencycoda/mia-table/src/lib/mia-table.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MiaTableComponent } from './mia-table.component';
+
+
+
+@NgModule({
+  declarations: [MiaTableComponent],
+  imports: [
+  ],
+  exports: [MiaTableComponent]
+})
+export class MiaTableModule { }
---
+++ projects/agencycoda/mia-table/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of mia-table
+ */
+
+export * from './lib/mia-table.service';
+export * from './lib/mia-table.component';
+export * from './lib/mia-table.module';
